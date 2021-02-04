83d2ca5c35c91e5852df3fa68a19d69f60869384
Christian Hall <christian@Christians-MacBook-Pro.local>
2021-02-03 21:21:24 -0500

    added paddle functionality

---
+++ src/pong/HumanPaddle.java
@@ -0,0 +1,69 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class HumanPaddle implements Paddle{
+	double y;
+	double yVel;
+	boolean upAccel;
+	boolean downAccel;
+	final double GRAVITY = 0.94;
+	int player;
+	int x;
+	
+	public HumanPaddle (int player) {
+		upAccel = false;
+		downAccel = false;
+		y = 210;
+		yVel = 0;
+		if (player == 1) {
+			x = 20;
+		} else {
+			x = 660;
+		}
+		
+	}
+
+	public void draw(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(x, (int)y, 20, 80);
+		
+	}
+
+	public void move() {
+		if (upAccel) {
+			yVel -= 2; // change this number to increase or decrease paddle speed
+		} else if (downAccel) {
+			yVel += 2; 
+
+		} else if (!upAccel && !downAccel) {
+			yVel *= GRAVITY; // change gravity to affect slowdown
+		}
+		if (yVel >= 5) { // these set a speed limit
+			yVel = 5;
+		} else if (yVel <= -5) {
+			yVel = -5;
+		}
+		y += yVel;
+		
+		if (y < 0) { // keeps the paddles on the page
+			y = 0;
+		} else if (y > 420) { // 500 height - 80 paddle height
+			y = 420;
+		}
+	}
+	
+	public void setUpAccel(boolean input) {
+		upAccel = input;
+	}
+	
+	public void setDownAccel(boolean input) {
+		downAccel = input;
+	}
+
+	public int getY() {
+		return (int)y;
+	}
+
+}
--- src/pong/Tennis.java
+++ src/pong/Tennis.java
@@ -10,10 +10,12 @@ public class Tennis extends Applet implements Runnable, KeyListener {
 	final int WIDTH = 700;
 	final int HEIGHT = 500;
 	Thread thread;
+	HumanPaddle p1;
 	
 	public void init() {
 		this.resize(WIDTH, HEIGHT);
 		this.addKeyListener(this);
+		p1 = new HumanPaddle(2);
 		thread = new Thread(this);
 		thread.start();
 	}
@@ -21,7 +23,7 @@ public class Tennis extends Applet implements Runnable, KeyListener {
 	public void paint(Graphics g) {
 		g.setColor(Color.black);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
-		
+		p1.draw(g);
 	}
 	
 	public void update(Graphics g) {
@@ -30,7 +32,7 @@ public class Tennis extends Applet implements Runnable, KeyListener {
 
 	public void run() {
 		for (;;) {
-			
+			p1.move();
 			repaint();
 			try {
 				Thread.sleep(10);
@@ -41,23 +43,23 @@ public class Tennis extends Applet implements Runnable, KeyListener {
 
 		
 	}
-
+	// add another two e.getKeyCode() == to different keys for two players
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_UP) {
-			
+			p1.setUpAccel(true);
 		}
 		else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			
+			p1.setDownAccel(true);
 		}
 		
 	}
 
 	public void keyReleased(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_UP) {
-			
+			p1.setUpAccel(false);
 		}
 		else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			
+			p1.setDownAccel(false);
 		}
 		
 	}
